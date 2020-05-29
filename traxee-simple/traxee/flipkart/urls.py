56beddeb74dae32267ebc488eb67f3902a84fbf3
SRIJAN SINGH <41985103+srijansingh53@users.noreply.github.com>
2020-05-29 22:16:09 +0530

    track functionality done

--- traxee-simple/traxee/flipkart/urls.py
+++ traxee-simple/traxee/flipkart/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     url(r'^signup/$', views.signup_user, name='signup'),
     url(r'^logout/$', views.logout_user, name='logout'),
     url(r'^search/$', views.search_product, name='search'),
-    
+    url(r'^add_track/(?P<product_id>\w+)/$', views.add_track, name='add_track'),
+    url(r'^track/$', views.display_track, name='track'),
+    url(r'^(?P<product_id>\w+)/del_track/$', views.remove_track, name='delete_track'),
 ]
